39eab9c65f3b07db9f9a48ba4f837cd2f1984f32
woojoonk <woojoonk@andrew.cmu.edu>
2022-04-05 22:50:25 -0400

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,51 @@
+/*!
+* Start Bootstrap - Scrolling Nav v5.0.5 (https://startbootstrap.com/template/scrolling-nav)
+* Copyright 2013-2022 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-scrolling-nav/blob/master/LICENSE)
+*/
+//
+// Scripts
+// 
+
+
+setTimeout(function(){
+    document.getElementsByClassName("firstWord")[0].classList.add("loaded")
+},1000);
+
+setTimeout(function(){
+    document.getElementsByClassName("secondWord")[0].classList.add("loaded")
+},2000);
+
+setTimeout(function(){
+    document.getElementsByClassName("photo")[0].classList.add("loaded")
+},3000);
+
+setTimeout(function(){
+    document.getElementsByClassName("newButton")[0].classList.add("loaded")
+},4000);
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            offset: 74,
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
